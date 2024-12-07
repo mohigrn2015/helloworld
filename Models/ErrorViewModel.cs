83d61baebf2450cd05d11c3ce2c47ea7e3812f85
mohi_ssle <mohiuddin@naasbd.com>
2024-12-07 11:49:43 +0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace docker_with_ci_cd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
